9b8a08859a89e11768477c8b490a09eaa78d3f09
David Tacoronte <davidtacoronte@Davids-MBP.attlocal.net>
2022-10-17 11:55:07 -0400

    started roject and added some extra functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// EDIT 1
+let picfbHover = document.querySelector(".funbusimg");
+picfbHover.addEventListener('mouseover', (evt) => {
+    picfbHover.title = "We promise the Fun Bus is not a bus for ants!"
+});
+
+//EDIT 2
+let picViet = document.querySelector(".content-destination img");
+
+
+
+
+picViet.addEventListener('mouseover', (evt) => {
+    picViet.title = "Want to know where this is? CLICK ME!";
+
+});
+
+picViet.addEventListener('click', (evt) => {
+
+const vietImgLink = document.createElement("a");
+
+vietImgLink.src = "https://www.vietnamcoracle.com/phu-quy-island-travel-guide/";
+
+picViet.append
+});
